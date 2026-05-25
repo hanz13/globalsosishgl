65413207971fddfac195da05995418f20f304cc3
Mas Subhan <72450040+hanz13@users.noreply.github.com>
2026-05-25 22:16:52 +0700

    update label

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -33,10 +33,10 @@ export const AboutUs: React.FC = () => {
                 <div className="w-8 h-8 rounded-lg bg-rose-100 flex items-center justify-center text-rose-600">
                   <ShieldCheck className="w-5 h-5" />
                 </div>
-                <span className="text-xs font-bold text-stone-500 uppercase tracking-wider">Garansi/Return</span>
+                <span className="text-xs font-bold text-stone-500 uppercase tracking-wider">Garansi & Return</span>
               </div>
               <p className="text-sm font-bold text-stone-800 leading-tight">
-                Kemasan Segel Rusak, dan Isi Berubah Rasa.
+                KEMASAN SEGEL RUSAK DAN ISI BERUBAH RASA.
               </p>
             </div>
           </div>
